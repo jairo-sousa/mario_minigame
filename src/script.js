cd61029f54ccd8256afee223cebc0bc1365d46a0
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-08-31 20:36:10 -0300

    variáveis do jogo criadas

--- src/script.js
+++ src/script.js
@@ -1,1 +1,23 @@
 console.log("+++   Mario Minigame   +++");
+
+// VISÕES
+const visaoMenu = document.querySelector("#visaoMenu");
+const visaoJogo = document.querySelector("#visaoJogo");
+const visaoGameOver = document.querySelector("#visaoGameOver");
+
+// SPRITES
+const spriteJogador = document.querySelector("#spriteJogador");
+const spriteCano = document.querySelector("#spriteCano");
+const spriteNuvens = document.querySelector("#spriteNuvens");
+
+// BOTÕES
+const botaoStart = document.querySelector("#botaoStart");
+const botaoRestart = document.querySelector("#botaoRestart");
+const botaoQuit = document.querySelector("#botaoQuit");
+
+// AUDIOS
+const audioStart = new Audio("/audio/start.ogg");
+const audioEnd = new Audio("/audio/end.ogg");
+
+// BANDEIRA
+let jogoEstaRodando = false;
